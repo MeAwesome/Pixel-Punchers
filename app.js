0714e53786d35f83fbbd5819d856181fad718b6b
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-02-20 13:47:39 -0500

    Starting Code

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var serv = require("http").Server(app);
+var io = require("socket.io")(serv,{});
+var port = process.env.PORT;
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/public/index.html");
+});
+app.use("/client", express.static(__dirname + "/public"));
+
+serv.listen(port || 3000);
+
+io.on("connection", function(socket){
+	
+});
