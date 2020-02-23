54511130d30e457bf1e37b2e2b3fd78b154b0bbe
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-02-23 14:30:52 -0500

    Image Additions

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,7 @@ function setup(){
 
 socket.on("connected_to_server", (player) => {
   console.log("connected");
+  console.log(player.id);
   me.setPlayerNumber(player.playerNumber);
   me.setPlayerNickname(player.nickname);
 });
