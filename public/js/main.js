e0e414a1ce2f744b25bdda3d21b0fc0598d4118c
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-02-25 11:42:08 -0500

    testing boxes

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,10 @@ function onLoad(){
 function setup(){
   gameAreaBuffer.setSize(1280, 720);
   gameAreaBuffer.fill(Color.felicity);
-  gameAreaBuffer.box(100, 100, 400, 400, Color.white);
+  gameAreaBuffer.box(100, 100, 100, 100, Color.white);
+  gameAreaBuffer.box(1080, 100, 100, 100, Color.white);
+  gameAreaBuffer.box(100, 500, 100, 100, Color.white);
+  gameAreaBuffer.box(1080, 500, 100, 100, Color.white);
   gameAreaBuffer.setVisibility(false);
   gameArea.setSize(window.innerWidth, window.innerHeight);
   gameArea.copyData(gameAreaBuffer, 0, 0, gameArea.canvas.width, gameArea.canvas.height);
