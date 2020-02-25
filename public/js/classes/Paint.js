0be141de9348c0b253892280e619fc136f3621d0
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-02-25 11:22:31 -0500

    Updates To Paint and testing

--- public/js/classes/Paint.js
+++ public/js/classes/Paint.js
@@ -1,15 +1,16 @@
 function Paint(id){
-  this.canvas = document.createElement(canvas);
-  this.canvas.setAttribute("id", id);
+  this.canvas = document.createElement("canvas");
+  this.canvas.id = id;
   this.context = this.canvas.getContext("2d");
   this.context.imageSmoothingEnabled = true;
+  document.body.appendChild(this.canvas);
 
   this.setSize = function(w, h){
     this.canvas.width = w;
     this.canvas.height = h;
   }
 
-  this.setBuffer = function(bool){
+  this.setVisibility = function(bool){
     if(bool == true){
       this.canvas.style.display = "block";
     } else {
@@ -17,8 +18,41 @@ function Paint(id){
     }
   }
 
-  this.fill = function(color){
-    this.fillStyle = color;
-    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  this.clear = function(){
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
+
+  this.fill = function(color){
+    this._saveValues();
+    this._fillColor(color);
+    this._fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this._restoreValues();
+  }
+
+  this.box = function(x, y, w, h, color){
+    this._saveValues();
+    this._fillColor(color);
+    this._fillRect(x, y, w, h);
+    this._restoreValues();
+  }
+
+  this.copyData = function(paint, x, y, w, h){
+    this.context.drawImage(paint.canvas, 0, 0, paint.canvas.width, paint.canvas.height, x, y, w, h);
+  }
+
+  this._fillColor = function(color){
+    this.context.fillStyle = color;
+  }
+
+  this._fillRect = function(x, y, w, h){
+    this.context.fillRect(x, y, w, h);
+  }
+
+  this._saveValues = function(){
+		this.context.save();
+	}
+
+	this._restoreValues = function(){
+		this.context.restore();
+	}
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,29 @@
-const socket = io();
-const me = new Player();
-
-function setup(){
-
+function onLoad(){
+  socket = io();
+  bindSocketEvents();
+  me = new Player();
+  gameAreaBuffer = new Paint("gameAreaBuffer");
+  gameArea = new Paint("gameArea");
 }
 
-socket.on("connected_to_server", (player) => {
-  me.setPlayerNumber(player.playerNumber);
-  me.setPlayerNickname(player.nickname);
-  setup();
-});
+function setup(){
+  gameAreaBuffer.setSize(1280, 720);
+  gameAreaBuffer.fill(Color.felicity);
+  gameAreaBuffer.box(100, 100, 400, 400, Color.white);
+  gameAreaBuffer.setVisibility(false);
+  gameArea.setSize(window.innerWidth, window.innerHeight);
+  gameArea.copyData(gameAreaBuffer, 0, 0, gameArea.canvas.width, gameArea.canvas.height);
+  gameArea.setVisibility(true);
+}
+
+function bindSocketEvents(){
+  socket.on("connected_to_server", (player) => {
+    me.setPlayerNumber(player.playerNumber);
+    me.setPlayerNickname(player.nickname);
+    setup();
+  });
+}
+
+window.onload = function(){
+  onLoad();
+}
