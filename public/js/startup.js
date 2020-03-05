b852e823ee92d027b2dcfe2e09c2b2903584d1c1
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-03-05 12:21:24 -0500

    Update startup.js

--- public/js/startup.js
+++ public/js/startup.js
@@ -2,7 +2,7 @@ window.onload = function(){
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
   	document.getElementById("deviceScript").src = "/public/js/phone.js";
   } else {
-  	document.getElementById("deviceScript").src = "/public/js/phone.js";
+  	document.getElementById("deviceScript").src = "/public/js/display.js";
   }
   document.getElementById("deviceScript").onload = function(){
     onLoad();
