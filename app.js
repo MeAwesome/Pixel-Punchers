d72cda3fb9ddae7613b3115e932dfce331044f48
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-07-12 01:22:17 -0400

    Major Updates To Network Connectivity

--- app.js
+++ app.js
@@ -1,201 +1,66 @@
-var express = require("express");
-var os = require("os");
-var app = express();
-var serv = require("http").Server(app);
-var io = require("socket.io")(serv,{});
-var port = process.env.PORT || 52470;
-app.get("/", function(req, res){
+//Server Requires & Port Setup
+
+const express = require("express");
+const os = require("os");
+const app = express();
+const serv = require("http").Server(app);
+const io = require("socket.io")(serv,{});
+const port = process.env.PORT || 52470;
+
+//Custom Requires
+
+const _ = require("underscore");
+const Network = require("./server/classes/Network.js");
+
+//Server Setup & Initiation
+
+app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/public/index.html");
 });
 app.use("/public", express.static(__dirname + "/public"));
 serv.listen(port);
+
 if(port != process.env.PORT){
 	var __ConnectTo__ = os.networkInterfaces()["Wi-Fi"][1].address + ":" + port;
 	console.clear();
 	console.log("--> Webpage Started On } " + __ConnectTo__);
 }
 
+//Connection & Message Handling
 
+io.on("connection", (socket) => {
 
-var connections = {};
-var rooms = {};
-
-io.on("connection", function(socket){
-
-	connections[socket.id] = new Connection(socket);
+	new Network.connection(socket);
 
 	socket.emit("connected_to_server");
 
 	socket.on("disconnect", () => {
-		if(connections[socket.id].isHost){
-			rooms[connections[socket.id].room].sendToPlayers("host_disconnect");
-			rooms[connections[socket.id].room].removeAllPlayers();
-			delete rooms[connections[socket.id].room];
-			delete connections[socket.id];
-		} else {
-			if(connections[socket.id].room){
-				rooms[connections[socket.id].room].removePlayer(socket.id);
-			}
-			delete connections[socket.id];
-		}
+		Network.connections.getConnection(socket.id).terminate();
 	});
 
-	socket.on("create_room", (type) => {
-		var r = new Room(socket.id, type);
-		rooms[r.getCode()] = r;
-		socket.emit("room_metadata", r.getRoomMetadata());
+	socket.on("create_room", () => {
+		if(Network.connections.getConnection(socket.id).room == undefined){
+			var r = new Network.room();
+			Network.connections.getConnection(socket.id).joinRoom(r.getCode());
+		}
 	});
 
 	socket.on("join_room", (code) => {
-		if(code == "#RANDOM"){
-			for(var r = 0; r < Object.keys(rooms).length; r++){
-				if(rooms[Object.keys(rooms)[r]].canJoin()){
-					code = Object.keys(rooms)[r];
-				}
-			}
-		}
-		if(rooms[code] != undefined){
-			if(rooms[code].canJoin()){
-				rooms[code].addPlayer(socket.id);
-				connections[socket.id].updateRoomInfo(code, false);
-				if(rooms[code].totalPlayers() == 1){
-					rooms[code].sendToHost("first_player_connected");
-				}
-				socket.emit("connected_to_room");
-			} else {
-				socket.emit("room_unjoinable");
-			}
-		} else {
-			socket.emit("invalid_room");
-		}
+		Network.connections.getConnection(socket.id).joinRoom(code);
 	});
 
-	socket.on("update_room_metadata", (data) => {
-		rooms[connections[socket.id].room].updateMetadata(data);
+	socket.on("leave_room", () => {
+		Network.connections.getConnection(socket.id).leaveRoom();
 	});
 
-	socket.on("update_player_metadata", (data) => {
-		rooms[connections[socket.id].room].updatePlayerMetadata(socket.id, data);
+	socket.on("find_rooms", () => {
+		var open_rooms = [];
+		for(room in rooms){
+			if(rooms[room].canJoin()){
+				open_rooms.push(room);
+			}
+		}
+		socket.emit("found_rooms", open_rooms);
 	});
 
 });
-
-function Connection(socket){
-	this.socket = socket;
-	this.socketId = socket.id;
-	this.room = undefined;
-	this.isHost = false;
-	this.isPlayer = false;
-
-	this.updateRoomInfo = function(room, isHost){
-		this.room = room;
-		this.isHost = isHost;
-	}
-
-	this.getRoom = function(){
-		return this.room;
-	}
-}
-
-function Room(hostId, type){
-	this.metadata = {
-		host:hostId,
-		type:type,
-		code:generateRoomCode(),
-		players:{},
-		maxPlayers:8
-	};
-
-	this.data = {
-
-	}
-
-	this.addPlayer = function(playerId){
-		this.metadata.players[playerId] = new Player(playerId, Object.keys(this.metadata.players));
-		this.sendToHost("room_metadata", this.metadata);
-	}
-
-	this.removePlayer = function(playerId){
-		connections[playerId].room = undefined;
-		delete this.metadata.players[playerId];
-	}
-
-	this.removeAllPlayers = function(){
-		for(var p = 0; p < Object.keys(this.metadata.players).length; p++){
-			connections[this.metadata.players[Object.keys(this.metadata.players)[p]].id].room = undefined;
-		}
-		this.metadata.players = {};
-	}
-
-	this.totalPlayers = function(){
-		return Object.keys(this.metadata.players).length
-	}
-
-	this.canJoin = function(){
-		return  this.totalPlayers() < this.metadata.maxPlayers;
-	}
-
-	this.getRoomMetadata = function(){
-		return this.metadata;
-	}
-
-	this.getCode = function(){
-		return this.metadata.code;
-	}
-
-	this.sendToPlayers = function(name, data){
-		for(var p = 0; p < Object.keys(this.metadata.players).length; p++){
-			connections[this.metadata.players[Object.keys(this.metadata.players)[p]].id].socket.emit(name, data);
-		}
-	}
-
-	this.sendToHost = function(name, data){
-		connections[this.metadata.host].socket.emit(name, data);
-	}
-
-	this.updateMetadata = function(data){
-		for(var m = 0; m < Object.keys(data).length; m++){
-			this.metadata[Object.keys(data)[m]] = data[Object.keys(data)[m]];
-		}
-		this.sendToHost("room_metadata", this.metadata);
-	}
-
-	this.updatePlayerMetadata = function(player, data){
-		var me = this.metadata.players[player];
-		me.changeNickname(data.nickname);
-		this.sendToHost("room_metadata", this.metadata);
-	}
-
-	connections[hostId].updateRoomInfo(this.metadata.code, true);
-}
-
-function generateRoomCode(){
-	var code = "";
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  for(var i = 0; i < 4; i++){
-   	code += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-	for(var c = 0; c < Object.keys(rooms).length; c++){
-		if(code == rooms[Object.keys(rooms)[c]].getCode()){
-			generateRoomCode();
-			return;
-		}
-	}
-  return code;
-}
-
-function Player(socketId, players){
-	this.id = socketId;
-	this.playerNumber = players.length;
-	this.defaultNickname = "Player " + (players.length + 1);
-	this.nickname = "Player " + (players.length + 1);
-
-	this.changeNickname = function(name){
-		name = name.trim();
-		if(name == ""){
-			this.nickname = this.defaultNickname;
-		} else {
-			this.nickname = name;
-		}
-	}
-}
