21c78a577cde5e0fd447b77ca2a985a5ac4aa699
Isaac Robbins <isaacprobbins@gmail.com>
2020-11-06 23:53:19 -0500

    Update Color.js

--- src/public/js/Color.js
+++ src/public/js/Color.js
@@ -1,27 +1,17 @@
 const Color = {
-  white:"#FFFFFF",
-  grey:"#222222",
-  lightgrey:"#AAAAAA",
-  silver:"#C0C0C0",
-  black:"#000000",
-  red:"#FF0000",
-  green:"#00FF00",
-  blue:"#0000FF",
-  yellow:"#FFFF00",
-  orange:"#FF8000",
-  lightblue:"#007FFF",
-  teal:"#00FFFF",
-  purple:"#8B008B",
-  magenta:"#FF00FF",
-  felicity:"#008000",
-  toHex:function(rgb){
-    var hex = Number(rgb).toString(16);
-    if(hex.length < 2){
-         hex = "0" + hex;
-    }
-    return hex;
-  },
-  rgbToHex:function(r, g, b){
-    return Color.toHex(r) + Color.toHex(g) + Color.toHex(b);
-  }
+  white:0xFFFFFF,
+  grey:0x222222,
+  lightgrey:0xAAAAAA,
+  silver:0xC0C0C0,
+  black:0x000000,
+  red:0xFF0000,
+  green:0x00FF00,
+  blue:0x0000FF,
+  yellow:0xFFFF00,
+  orange:0xFF8000,
+  lightblue:0x007FFF,
+  teal:0x00FFFF,
+  purple:0x8B008B,
+  magenta:0xFF00FF,
+  felicity:0x008000
 }
